6290271feb44b8b5f81193afb3848de08bdfc9fa
ArnaudGadbin <gadbin.arnaud8@gmail.com>
2024-06-04 14:41:21 +0200

    fix new api routes

--- front/moodboard/src/Api.js
+++ front/moodboard/src/Api.js
@@ -1,21 +1,25 @@
 import axios from "axios"
 
 async function getMoodBoard() {
-  return await axios.get("http://localhost:8080/elements")
+  return await axios.get("http://localhost:8080/api/elements")
 }
 
 async function addText(content) {
-  return await axios.post("http://localhost:8080/elements/create", {
+  return await axios.post("http://localhost:8080/api/elements", {
     type: "text",
     content
   })
 }
 
 async function addImage() {
-  return await axios.post("http://localhost:8080/elements/create", {
+  return await axios.post("http://localhost:8080/api/elements", {
     type: "Image",
     content: "https://source.unsplash.com/300x300"
   })
+}
+
+async function deleteElement(id) {
+  return await axios.delete(`http://localhost:8080/api/element/${id}`)
 }
 
 export { getMoodBoard, addText, addImage }
